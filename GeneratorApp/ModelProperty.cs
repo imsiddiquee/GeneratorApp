0fe58d34829fbbe5effcb7c7145c00b001695241
Shamim <imsiddiquee@gmail.com>
2019-05-08 16:32:43 +0600

    first commit

---
+++ GeneratorApp/ModelProperty.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneratorApp
+{
+    public class ModelProperty
+    {
+        public int OrderId { get; set; }
+        public string PropertyType { get; set; }
+        public string PropertyName { get; set; }
+        public string ModelName { get; set; }
+    }
+}
---
+++ GeneratorApp/frmGeneratorApp.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace GeneratorApp
+{
+    partial class frmGeneratorApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtModelName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Path :";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(84, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(358, 20);
+            this.txtFilePath.TabIndex = 1;
+            this.txtFilePath.Text = "G:\\Gene";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(84, 74);
+            this.txtModel.Multiline = true;
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(358, 224);
+            this.txtModel.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Model";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(258, 320);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate Service";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(367, 320);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtModelName
+            // 
+            this.txtModelName.Location = new System.Drawing.Point(84, 36);
+            this.txtModelName.Name = "txtModelName";
+            this.txtModelName.Size = new System.Drawing.Size(358, 20);
+            this.txtModelName.TabIndex = 7;
+            this.txtModelName.Text = "test";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Model Name :";
+            // 
+            // frmGeneratorApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 358);
+            this.Controls.Add(this.txtModelName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label1);
+            this.Name = "frmGeneratorApp";
+            this.Text = "Generator App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtModelName;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
