51b081eb87e9d232fe0caa177b6f026a4578306a
Zahid Iqbal Tushar <zi.tushar@live.com>
2019-05-10 10:31:50 +0600

    minor update generate app service

--- GeneratorApp/GenerateAppService.cs
+++ GeneratorApp/GenerateAppService.cs
@@ -75,7 +75,7 @@ namespace GeneratorApp
                     sw.WriteLine("_" + modelName.ToLower() + "Repository.Delete(_objectMapper.Map<Entities.Setup." + modelName + ">(" + modelName.ToLower() + "));");
                     sw.WriteLine("}");
 
-                    sw.WriteLine("public void Delete(int id)");
+                    sw.WriteLine("public void DeleteById(int id)");
                     sw.WriteLine("{");
                     sw.WriteLine("_" + modelName.ToLower() + "Repository.Delete(id);");
                     sw.WriteLine("}");
