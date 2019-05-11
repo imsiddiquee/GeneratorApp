9572795f6cf12e7881bf724007efd4022429f4fd
Zahid Iqbal Tushar <zi.tushar@live.com>
2019-05-11 12:51:40 +0600

    update to include queryObject

--- GeneratorApp/GenerateAppService.cs
+++ GeneratorApp/GenerateAppService.cs
@@ -26,10 +26,15 @@ namespace GeneratorApp
                     sw.WriteLine("using System.Collections.Generic;");
                     sw.WriteLine("using System.Linq;");
                     sw.WriteLine("");
+
                     sw.WriteLine("namespace HRIS.Setup."+modelName+"");
                     sw.WriteLine("{");
+
+                    sw.WriteLine("[RemoteService(false)]");
                     sw.WriteLine("public class "+ modelName + "AppService : ApplicationService, I"+ modelName + "AppService");
                     sw.WriteLine("{");
+
+
                     sw.WriteLine("private readonly IObjectMapper _objectMapper;");
                     sw.WriteLine("private  readonly IRepository<Entities.Setup."+ modelName + "> _" + modelName.ToLower() + "Repository;");
                     sw.WriteLine("");
@@ -39,49 +44,69 @@ namespace GeneratorApp
                     sw.WriteLine("_" + modelName.ToLower() + "Repository = " + modelName.ToLower() + "Repository;");
                     sw.WriteLine("}");
                     sw.WriteLine("");
-                    sw.WriteLine("public " + modelName + "Dto Create(" + modelName + "Dto " + modelName.ToLower() + ")");
+
+                    sw.WriteLine("public async Task<" + modelName + "Dto> Create(" + modelName + "Dto " + modelName.ToLower() + ")");
                     sw.WriteLine("{");
                     sw.WriteLine("var " + modelName.ToLower() + "Obj = _objectMapper.Map<Entities.Setup." + modelName + ">(" + modelName.ToLower() + ");");
                     sw.WriteLine("" + modelName.ToLower() + "Obj.TenantId = 2;");
-                    sw.WriteLine("return _objectMapper.Map<" + modelName + "Dto>(_" + modelName.ToLower() + "Repository.Insert(" + modelName.ToLower() + "Obj));");
+                    sw.WriteLine("return _objectMapper.Map<" + modelName + "Dto>(await _" + modelName.ToLower() + "Repository.Insert(" + modelName.ToLower() + "Obj));");
                     sw.WriteLine("}");
+
                     sw.WriteLine("");
-                    sw.WriteLine("public " + modelName + "Dto GetById(int id)");
+
+                    sw.WriteLine("public async Task<" + modelName + "Dto> GetById(int id)");
                     sw.WriteLine("{");
-                    sw.WriteLine("var result = _" + modelName.ToLower() + "Repository");
-                    sw.WriteLine(".GetAllList()");
-                    sw.WriteLine(".FirstOrDefault(w => w.Id == id && w.IsActive == true && w.IsDeleted == false);");
+                    sw.WriteLine("var result = await _" + modelName.ToLower() + "Repository");
+                    //sw.WriteLine(".GetAllList()");
+                    sw.WriteLine(".FirstOrDefaultAsync(w => w.Id == id && w.IsDeleted == false);");
                     sw.WriteLine("");
                     sw.WriteLine("return _objectMapper.Map<" + modelName + "Dto>(result);");
                     sw.WriteLine("}");
 
-                    sw.WriteLine("public List<" + modelName + "Dto> GetAll()");
-                    sw.WriteLine("{");
-                    sw.WriteLine("var result = _" + modelName.ToLower() + "Repository");
-                    sw.WriteLine(".GetAll()");
-                    sw.WriteLine(".Where(w => w.IsActive == true && w.IsDeleted == false).ToList();");
                     sw.WriteLine("");
-                    sw.WriteLine("return _objectMapper.Map<List<" + modelName + "Dto>>(result);");
+
+                    sw.WriteLine("public async Task<IEnumerable<" + modelName + "Dto>> GetAll(IQueryObject queryObject)");
+                    sw.WriteLine("{");
+                    sw.WriteLine("var query = _" + modelName.ToLower() + "Repository");
+                    sw.WriteLine(".GetAll()");
+                    sw.WriteLine(".Where(w => w.IsDeleted == false).AsQueryable();");
+                    sw.WriteLine("");
+                    sw.WriteLine("var columnsMap = new Dictionary<string, Expression<Func<Entities.Setup." + modelName + ", object>>>");
+                    sw.WriteLine("{");
+                    sw.WriteLine("[\"Name\"] = v => v.Name,");
+                    sw.WriteLine("[\"Code\"] = v => v.Code,");
+                    sw.WriteLine("};");
+                    sw.WriteLine("query = query.ApplyOrdering(queryObject, columnsMap);");
+                    sw.WriteLine("var result = await query.ApplyPaging(queryObject).ToListAsync();");
+                    sw.WriteLine("return _objectMapper.Map<IEnumerable<" + modelName + "Dto>>(result);");
                     sw.WriteLine("}");
 
-                    sw.WriteLine("public " + modelName + "Dto Update(" + modelName + "Dto " + modelName.ToLower() + ")");
+                    sw.WriteLine("");
+
+                    sw.WriteLine("public async Task<" + modelName + "Dto> Update(" + modelName + "Dto " + modelName.ToLower() + ")");
                     sw.WriteLine("{");
                     sw.WriteLine("var result = _objectMapper.Map<Entities.Setup." + modelName + ">(" + modelName.ToLower() + ");");
-                    sw.WriteLine("return _objectMapper.Map<" + modelName + "Dto>(_" + modelName.ToLower() + "Repository.Update(result));");
+                    sw.WriteLine("return _objectMapper.Map<" + modelName + "Dto>(await _" + modelName.ToLower() + "Repository.Update(result));");
                     sw.WriteLine("}");
 
-                    sw.WriteLine("public void Delete(" + modelName + "Dto " + modelName.ToLower() + ")");
+                    sw.WriteLine("");
+
+                    //sw.WriteLine("public void Delete(" + modelName + "Dto " + modelName.ToLower() + ")");
+                    //sw.WriteLine("{");
+                    //sw.WriteLine("_" + modelName.ToLower() + "Repository.Delete(_objectMapper.Map<Entities.Setup." + modelName + ">(" + modelName.ToLower() + "));");
+                    //sw.WriteLine("}");
+
+                    sw.WriteLine("public async Task DeleteById(int id)");
                     sw.WriteLine("{");
-                    sw.WriteLine("_" + modelName.ToLower() + "Repository.Delete(_objectMapper.Map<Entities.Setup." + modelName + ">(" + modelName.ToLower() + "));");
+                    sw.WriteLine("await _" + modelName.ToLower() + "Repository.Delete(id);");
                     sw.WriteLine("}");
 
-                    sw.WriteLine("public void DeleteById(int id)");
-                    sw.WriteLine("{");
-                    sw.WriteLine("_" + modelName.ToLower() + "Repository.Delete(id);");
-                    sw.WriteLine("}");
+                    sw.WriteLine("");
 
-                    sw.WriteLine("}");
-                    sw.WriteLine("}");
+                    sw.WriteLine("}");// end class
+
+                    sw.WriteLine("}");//end namespace
+
 
                 }
             }
